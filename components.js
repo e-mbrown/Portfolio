8d5b1ed539c50ef516c5ad55e7af81256094f2ec
e-mbrown <ebsonari@gmail.com>
2020-10-06 19:16:10 -0400

    Playing with layout of about page

--- components.js
+++ components.js
@@ -2,7 +2,7 @@ class Header extends HTMLElement {
     constructor(){
         super()
         this.innerHTML = `
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light ">
           <a class="navbar-brand" id="brand" href="#">Ebony Brown</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
